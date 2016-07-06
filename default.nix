c89f958dd7428bee39981ba7789ee6d9d35e753d
Neil Mayhew <neil_mayhew@sil.org>
2016-07-06 16:27:08 -0600

    Nixize

---
+++ default.nix
@@ -0,0 +1,16 @@
+{ mkDerivation, attoparsec, base, blaze-html, bytestring, filepath
+, hashable, HTTP, MissingH, network, network-uri, stdenv, unordered-containers
+}:
+mkDerivation {
+  pname = "Analytics";
+  version = "0.1.0.0";
+  src = ./.;
+  isLibrary = false;
+  isExecutable = true;
+  executableHaskellDepends = [
+    attoparsec base blaze-html bytestring filepath hashable HTTP
+    MissingH network network-uri unordered-containers
+  ];
+  description = "Analyze apache log files from a Debian repo";
+  license = stdenv.lib.licenses.gpl3;
+}
