589445f1c272bea5b405ee0d29fe85830cfd5a99
Neil Mayhew <neil@neil.mayhew.name>
2017-09-07 07:43:47 -0600

    Refactor Histogram to provide a pre-counted interface

--- Histogram.hs
+++ Histogram.hs
@@ -1,15 +1,18 @@
 {-# OPTIONS_GHC -Wno-type-defaults #-}
 
-module Histogram (histogram) where
+module Histogram where
 
 import Control.Arrow ((&&&))
 import Data.List (group, sort)
 import Text.Printf (printf)
 
 histogram ::  (Eq a, Ord a, Show a) => Int -> [a] -> [String]
-histogram cols xs = map format $ zip3 items freqs bars
+histogram cols = histogram' cols . count
+
+histogram' ::  (Show a, Show b, Real b) => Int -> [(a, b)] -> [String]
+histogram' cols prs = map format $ zip3 items freqs bars
   where
-    (items, freqs) = unzip $ count xs
+    (items, freqs) = unzip prs
     format (i, f, b) = printf "%*s: %*s %s"
         (width items) (show i) (width freqs) (show f) b :: String
     bars = map (bar . normalize) freqs
